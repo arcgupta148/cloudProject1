5599751a887b89106d97d039e335ccffd6ea5964
Archit Gupta <architgupta@Archits-Air.lan>
2024-10-17 20:23:36 -0400

    Initial commit

---
+++ assets/js/chat.js
@@ -0,0 +1,116 @@
+var checkout = {};
+
+$(document).ready(function() {
+  var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+  // Initialize mCustomScrollbar on window load
+  $(window).on('load', function() {
+    $messages.mCustomScrollbar(); // Initialize custom scrollbar
+    insertResponseMessage('Hi there, I\'m your personal Concierge. How can I help?');
+  });
+
+  // Function to update scrollbar position
+  function updateScrollbar() {
+    $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+      scrollInertia: 10,
+      timeout: 0
+    });
+  }
+
+  function setDate() {
+    d = new Date();
+    if (m != d.getMinutes()) {
+      m = d.getMinutes();
+      $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    }
+  }
+
+  function callChatbotApi(message) {
+    // Call your chatbot API
+    return sdk.chatbotPost({}, {
+      messages: [{
+        type: 'unstructured',
+        unstructured: {
+          text: message
+        }
+      }]
+    }, {});
+  }
+
+  function insertMessage() {
+    msg = $('.message-input').val();
+    if ($.trim(msg) === '') {
+      return false; // Prevent empty messages
+    }
+    $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    $('.message-input').val(null);
+    updateScrollbar(); // Update scrollbar after inserting message
+
+    callChatbotApi(msg)
+      .then((response) => {
+        console.log(response);
+        var data = response.data;
+
+        if (data.messages && data.messages.length > 0) {
+          console.log('Received ' + data.messages.length + ' messages');
+
+          var messages = data.messages;
+
+          for (var message of messages) {
+            if (message.type === 'unstructured') {
+              insertResponseMessage(message.unstructured.text);
+            } else if (message.type === 'structured' && message.structured.type === 'product') {
+              var html = '';
+
+              insertResponseMessage(message.structured.text);
+
+              setTimeout(function() {
+                html = '<img src="' + message.structured.payload.imageUrl + '" width="200" height="240" class="thumbnail" /><b>' +
+                  message.structured.payload.name + '<br>$' +
+                  message.structured.payload.price +
+                  '</b><br><a href="#" onclick="' + message.structured.payload.clickAction + '()">' +
+                  message.structured.payload.buttonLabel + '</a>';
+                insertResponseMessage(html);
+              }, 1100);
+            } else {
+              console.log('Not implemented');
+            }
+          }
+        } else {
+          insertResponseMessage('Oops, something went wrong. Please try again.');
+        }
+      })
+      .catch((error) => {
+        console.log('An error occurred', error);
+        insertResponseMessage('Oops, something went wrong. Please try again.');
+      });
+  }
+
+  $('.message-submit').click(function() {
+    insertMessage();
+  });
+
+  $(window).on('keydown', function(e) {
+    if (e.which == 13) {
+      insertMessage();
+      return false;
+    }
+  });
+
+  function insertResponseMessage(content) {
+    $('<div class="message loading new"><figure class="avatar"><img src="https://media.tenor.com/images/4c347ea7198af12fd0a66790515f958f/tenor.gif" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+    updateScrollbar();
+
+    setTimeout(function() {
+      $('.message.loading').remove();
+      $('<div class="message new"><figure class="avatar"><img src="https://media.tenor.com/images/4c347ea7198af12fd0a66790515f958f/tenor.gif" /></figure>' + content + '</div>').appendTo($('.mCSB_container')).addClass('new');
+      setDate();
+      updateScrollbar(); // Update scrollbar after adding response message
+      i++;
+    }, 500);
+  }
+
+});
